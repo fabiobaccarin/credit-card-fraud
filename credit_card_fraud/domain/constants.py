1be347da3a24a9a6ea71d9bb325b46d792ad39bb
Fabio Baccarin <fabiogbaccarin@gmail.com>
2025-11-29 11:42:01 -0300

    Update constants.py
    add MLFlow constants

--- credit_card_fraud/domain/constants.py
+++ credit_card_fraud/domain/constants.py
@@ -1,5 +1,10 @@
 """Project constants"""
 
-from typing import Final
+from typing import Final, NamedTuple
 
 OPENML_DATASET_ID: Final = 1597
+
+
+class MLFlow(NamedTuple):
+    EXPERIMENT_NAME: str = "Credit card fraud"
+    TRACKING_URI: str = "http://localhost:5000"
